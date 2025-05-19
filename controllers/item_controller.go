ff4abc9d9ada5854c430674d372df2114aee8ee8
abi01820 <111218880+riku04022@users.noreply.github.com.>
2025-05-19 20:03:01 +0900

    FindByIdの実装

--- controllers/item_controller.go
+++ controllers/item_controller.go
@@ -4,10 +4,12 @@ import (
 	"gin-sample/services"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type IItemController interface {
 	FindAll(ctx *gin.Context)
+	FindById(ctx *gin.Context)
 }
 
 type ItemController struct {
@@ -26,4 +28,24 @@ func (c *ItemController) FindAll(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{"data": items})
+}
+
+func (c *ItemController) FindById(ctx *gin.Context) {
+	itemId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		return
+	}
+
+	item, err := c.service.FindById(uint(itemId))
+	if err != nil {
+		if err.Error() == "Item not found" {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": item})
 }
