0b28b3566fac4684d190592d4a7f9498d38a736e
abi01820 <111218880+riku04022@users.noreply.github.com.>
2025-05-19 19:47:56 +0900

    itemsの実装

---
+++ controllers/item_controller.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"gin-sample/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type IItemController interface {
+	FindAll(ctx *gin.Context)
+}
+
+type ItemController struct {
+	service services.IItemService
+}
+
+func NewItemController(service services.IItemService) IItemController {
+	return &ItemController{service: service}
+}
+
+func (c *ItemController) FindAll(ctx *gin.Context) {
+	items, err := c.service.FindAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": items})
+}
